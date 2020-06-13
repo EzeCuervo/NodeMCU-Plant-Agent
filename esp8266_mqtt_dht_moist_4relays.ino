9df652c9aee4aef36ae370ad9315ee4e82712b5c
ezecuervo <ezecuervo.cod@gmail.com>
2020-06-13 17:09:20 -0300

    Discovery via HA / Some improvements on soil moisture read

--- esp8266_mqtt_dht_moist_4relays.ino
+++ esp8266_mqtt_dht_moist_4relays.ino
@@ -2,25 +2,32 @@
  *  ESP8266 + DHT11/22 + MOISTURE SENSOR + 4 RELAYS + MQTT + Home Assistant
  *  Author: EzeLibrandi
  *  Based on: https://github.com/projetsdiy/esp8266-dht22-mqtt-home-assistant
- *  Date: 05/09/2020
- *  Version 1.0
+ *  Date: 06/13/2020
+ *  Version 1.1.0
  *  Tested and running with the following libraries/boards:
  *  DHT Library v1.3.1 
  *  Adafruit Unified Sensors v1.0.3
- *  ES9266 Board v2.6.1
+ *  ES8266 Board v2.6.1
  *  Board on Arduino IDE: NodeMCU 1.0
  *  
  *  Lolin NodeMCUv3 - CH340 Driver
+ *  IMPORTANT: You MUST change MQTT_MAX_PACKET_SIZE to 256 on \Arduino\libraries\PubSubClient\src\PubSubClient.h
+ *  https://github.com/knolleary/pubsubclient#limitations
+ *  
+ *  NOTE: Before upload you have to change wifi/mqtt and place on mqtt topics. i.e. place could be bedroom
  */
-
-// NOTE: Before upload you have to change wifi/mqtt and place on mqtt topics. i.e. place could be bedroom
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include "DHT.h"
+#include <ArduinoJson.h>
 
 //Set debug mode (true = display log message via serial)
-bool debug = false;  
+bool debug = false;
+
+// Client name
+#define client_name "ESP8266-01"
+String place = "place";
 
 // Wifi Settings
 #define wifi_ssid "wifi ssid name"
@@ -31,58 +38,57 @@ bool debug = false;
 #define mqtt_user "mqtt username"
 #define mqtt_password "mqtt password"
 
-//MQTT Topics
-#define temperature_topic "place/sensor/temperature"
-#define humidity_topic "place/sensor/humidity"
-#define moisture_topic "place/sensor/moisture"
-
 //Buffer to decode MQTT messages
 char message_buff[100];
-long lastMsg = 0;   
+long lastMsg = 0;
+long lastSoil = 0;
+int moist_readeable = 0;
 long lastRecu = 0;
 
 //DHTPIN and DHTTYPE Settings ( Un-comment you sensor)
-#define DHTPIN D1               
-#define DHTTYPE DHT11           // DHT 11 
-//#define DHTTYPE DHT22         // DHT 22  (AM2302)
+#define DHTPIN D1
+#define DHTTYPE DHT11       // DHT 11
+//#define DHTTYPE DHT22 // DHT 22  (AM2302)
 
 // Assign output variables to GPIO pins for relays
-const int switch1 = 4;          // GPIO5 D2
-const int switch2 = 13;         // GPIO2 D7
-const int switch3 = 14;         // GPIO4 D5
-const int switch4 = 12;         // GPIO0 D6
-const int moist_sensor_pin = A0;// A0
+const int switch1 = 4;           // GPIO5 D2
+const int switch2 = 13;          // GPIO2 D7
+const int switch3 = 14;          // GPIO4 D5
+const int switch4 = 12;          // GPIO0 D6
+const int moist_sensor_pin = A0; // A0
 
 // Create abjects and commit setup
-DHT dht(DHTPIN, DHTTYPE);     
+DHT dht(DHTPIN, DHTTYPE);
 WiFiClient espClient;
 PubSubClient client(espClient);
-
-void setup() {
-  Serial.begin(9600);     
+void setup()
+{
+  Serial.begin(9600);
   //initialize the switch as an output and set to LOW (off)
-  pinMode(switch1, OUTPUT);               // Relay Switch 1
+  pinMode(switch1, OUTPUT); // Relay Switch 1
   digitalWrite(switch1, LOW);
-  pinMode(switch2, OUTPUT);               // Relay Switch 2
+  pinMode(switch2, OUTPUT); // Relay Switch 2
   digitalWrite(switch2, LOW);
-  pinMode(switch3, OUTPUT);               // Relay Switch 3
+  pinMode(switch3, OUTPUT); // Relay Switch 3
   digitalWrite(switch3, LOW);
-  pinMode(switch4, OUTPUT);               // Relay Switch 4
+  pinMode(switch4, OUTPUT); // Relay Switch 4
   digitalWrite(switch4, LOW);
-  setup_wifi();                           // Connect to Wifi network
-  client.setServer(mqtt_server, 1883);    // Configure MQTT connection
-  client.setCallback(callback);           // Callback function to execute when a MQTT message   
+  setup_wifi();                        // Connect to Wifi network
+  client.setServer(mqtt_server, 1883); // Configure MQTT connection
+  client.setCallback(callback);        // Callback function to execute when a MQTT message
   dht.begin();
 }
 
 //Conect to WiFi
-void setup_wifi() {
+void setup_wifi()
+{
   delay(10);
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(wifi_ssid);
   WiFi.begin(wifi_ssid, wifi_password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -93,16 +99,75 @@ void setup_wifi() {
 }
 
 //Try-reconnect function
-void reconnect() {
-  while (!client.connected()) {
+void reconnect(){
+  StaticJsonDocument<300> jsonBuffer;
+  while (!client.connected()){
     Serial.print("Connecting to MQTT broker... ");
-    if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
+    if (client.connect(client_name, mqtt_user, mqtt_password)){
       Serial.println("Connected OK!");
-      client.subscribe("place/switch1");
-      client.subscribe("place/switch2");
-      client.subscribe("place/switch3");
-      client.subscribe("place/switch4");
-    } else {
+      // MQTT Auto Discover for all switches and send actual status
+      for (int i = 1; i < 5; i++){
+        String switchActual = "switch";
+        switchActual.concat(i);
+        StaticJsonDocument<300> jsonBuffer;
+        jsonBuffer["name"] = place + " " + switchActual;
+        jsonBuffer["uniq_id"] = place + "_" + switchActual;
+        jsonBuffer["cmd_t"] = ("homeassistant/switch/" + place + "/" + switchActual + "/set");
+        jsonBuffer["stat_t"] = ("homeassistant/switch/" + place + "/" + switchActual + "/state");
+        char JSONmessageBuffer[300];
+        serializeJson(jsonBuffer, JSONmessageBuffer);
+        client.publish(("homeassistant/switch/" + place + "/" + switchActual + "/config").c_str(), JSONmessageBuffer, true);
+        int readedValue = digitalRead(atoi(switchActual.c_str()));
+        if (debug){
+          Serial.print("Switch: ");
+          Serial.print(switchActual);
+          Serial.print(" Readed value: ");
+          Serial.println(readedValue);
+        }
+        if (readedValue == 1){
+          client.publish(("homeassistant/switch/" + place + "/" + switchActual + "/state").c_str(), "OFF", true);
+        } else {
+          client.publish(("homeassistant/switch/" + place + "/" + switchActual + "/state").c_str(), "ON", true);
+        }
+        client.subscribe(("homeassistant/switch/" + place + "/" + switchActual + "/set").c_str());
+      }
+
+      //MQTT Topic - Temp
+      StaticJsonDocument<300> JSONTemp;
+      JSONTemp["dev_cla"] = "temperature";
+      JSONTemp["name"] = place + " Temperature";
+      JSONTemp["uniq_id"] = place + "_temperature";
+      JSONTemp["unit_of_meas"] = "°C";
+      JSONTemp["val_tpl"] = "{{ value_json.temperature}}";
+      JSONTemp["stat_t"] = "homeassistant/sensor/" + place + "/sensors/state";
+      char JSONmessageBuffer[300];
+      serializeJson(JSONTemp, JSONmessageBuffer);
+      client.publish(("homeassistant/sensor/" + place + "/sensors/config").c_str(), JSONmessageBuffer, true);
+
+      //MQTT Topic - Humidity
+      StaticJsonDocument<300> JSONHum;
+      JSONHum["dev_cla"] = "humidity";
+      JSONHum["name"] = place + " Humidity";
+      JSONHum["uniq_id"] = place + "_humidity";
+      JSONHum["unit_of_meas"] = "%";
+      JSONHum["val_tpl"] = "{{ value_json.humidity}}";
+      JSONHum["stat_t"] = "homeassistant/sensor/" + place + "/sensors/state";
+      serializeJson(JSONHum, JSONmessageBuffer);
+      client.publish(("homeassistant/sensor/" + place + "/sensorH/config").c_str(), JSONmessageBuffer, true);
+
+      //MQTT Topic - Soil Moisture
+      StaticJsonDocument<300> JSONSoilMoist;
+      JSONSoilMoist["dev_cla"] = "humidity";
+      JSONSoilMoist["name"] = place + " Soil Moist";
+      JSONSoilMoist["uniq_id"] = place + "_soilm";
+      JSONSoilMoist["unit_of_meas"] = "%";
+      JSONSoilMoist["val_tpl"] = "{{ value_json.soilm}}";
+      JSONSoilMoist["stat_t"] = "homeassistant/sensor/" + place + "/sensors/state";
+      serializeJson(JSONSoilMoist, JSONmessageBuffer);
+      client.publish(("homeassistant/sensor/" + place + "/sensorSM/config").c_str(), JSONmessageBuffer, true);
+    }
+    else
+    {
       Serial.print("Error: ");
       Serial.println(client.state());
       Serial.println("Wait 5 seconds before to retry...");
@@ -112,100 +177,128 @@ void reconnect() {
 }
 
 //Loop main code
-void loop() {
-  if (!client.connected()) {
+void loop(){
+  if (!client.connected()){
     reconnect();
   }
-  client.loop();  
+  client.loop();
   long now = millis();
   // Send a message every minute
-  if (now - lastMsg > 1000 * 60) {
+  if (now - lastMsg > 1000 * 60){
     lastMsg = now;
     // Read humidity & temp (Celcius)
     float h = dht.readHumidity();
     float t = dht.readTemperature();
+
     // Oh, nothing to send
-    if ( isnan(t) || isnan(h)) {
-      Serial.println("No info, please check DHT sensor!"); 
-    } else {
-      if ( debug ) {
-        Serial.print("Temperature: ");
-        Serial.print(t);
-        Serial.print(" & Humidity: ");
-        Serial.println(h);
-      }
-      // Publish temp / humidity sensors via MQTT 
-      client.publish(temperature_topic, String(t).c_str(), true);
-      client.publish(humidity_topic, String(h).c_str(), true);
+    if (isnan(t) || isnan(h)) {
+      Serial.println("No info, please check DHT sensor!");
+      return;
+    }
+    if (debug){
+      Serial.print("Temperature: ");
+      Serial.print(t);
+      Serial.print(" & Humidity: ");
+      Serial.print(h);
     }
     // Read moisture sensor
-    int moist = analogRead(moist_sensor_pin);
-    int moist_readeable = map(moist,1024,300,0,100);
-    // Oh, nothing to send
-    if ( isnan(moist)) {
-      Serial.println("No info, please check moisture sensor!");
-    } else {
-      if( debug ){  
-          Serial.print("Moisture: "); 
-          Serial.println(moist_readeable);
+    if (now - lastSoil > 1000 * 60 * 5 || moist_readeable < 70){
+      lastSoil = now;
+      int moist = analogRead(moist_sensor_pin);
+      moist_readeable = map(moist, 1024, 300, 0, 100);
+      // Oh, nothing to send
+      if (isnan(moist)){
+        Serial.println("No info, please check moisture sensor!");
       }
-      client.publish(moisture_topic, String(moist_readeable).c_str(), true);     
+      else{
+        if (debug){
+          Serial.print(" & Soil Moisture: ");
+          Serial.println(moist_readeable);
+        }
+      }
+    } else {
+      if (debug){
+        int minutesSinceLastSoilRead = (now - lastSoil) / 60;
+        Serial.print("Minutes since last Moisture read: " + minutesSinceLastSoilRead);
+      }
     }
+      //JSON with sensors info
+      StaticJsonDocument<300> jsonBuffer;
+      jsonBuffer["temperature"] = t;
+      jsonBuffer["humidity"] = h;
+      jsonBuffer["soilm"] = moist_readeable;
+      char JSONmessageBuffer2[300];
+      serializeJson(jsonBuffer, JSONmessageBuffer2);
+      if (debug){
+        Serial.print("Message: ");
+        Serial.println(JSONmessageBuffer2);
+      }
+      client.publish(("homeassistant/sensor/" + place + "/sensors/state").c_str(), JSONmessageBuffer2, true);
   }
-  if (now - lastRecu > 100 ) {
+  if (now - lastRecu > 100){
     lastRecu = now;
   }
 }
 
 // MQTT callback function
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length)
+{
   int i = 0;
-  for(i=0; i<length; i++) {
+  for (i = 0; i < length; i++)
+  {
     message_buff[i] = payload[i];
   }
   message_buff[i] = '\0';
   String message = String(message_buff);
-  
-  if ( debug ) {
+
+  if (debug){
     Serial.println("Topic: " + String(topic));
     Serial.println("Payload: " + message);
   }
-  
- // Check topic and message and set status on different pins
-  if (strcmp(topic,"place/switch1")==0){
-    if ( message == "on" ) {
-      digitalWrite(switch1,HIGH);
-      client.publish("place/switch1/state", "on");
-    } else if (message == "off"){
-      digitalWrite(switch1,LOW);  
-      client.publish("place/switch1/state", "off");
+
+  // Check topic and message and set status on different pins
+  if (strcmp(topic, ("homeassistant/switch/" + place + "/switch1/set").c_str()) == 0){
+    if (message == "ON"){
+      digitalWrite(switch1, HIGH);
+      Serial.print(digitalRead(switch1));
+      client.publish(("homeassistant/switch/" + place + "/switch1/state").c_str(), "ON", true);
+    }
+    else if (message == "OFF"){
+      digitalWrite(switch1, LOW);
+      Serial.print(digitalRead(switch1));
+      client.publish(("homeassistant/switch/" + place + "/switch1/state").c_str(), "OFF", true);
     }
   }
-  if (strcmp(topic,"place/switch2")==0){
-    if ( message == "on" ) {
-      digitalWrite(switch2,HIGH);
-      client.publish("place/switch2/state", "on");
-    } else if (message == "off"){
-      digitalWrite(switch2,LOW);
-      client.publish("place/switch2/state", "off");
+  if (strcmp(topic, ("homeassistant/switch/" + place + "/switch2/set").c_str()) == 0){
+    if (message == "ON"){
+      digitalWrite(switch2, HIGH);
+      client.publish(("homeassistant/switch/" + place + "/switch2/state").c_str(), "ON", true);
+    }
+    else if (message == "OFF"){
+      digitalWrite(switch2, LOW);
+      client.publish(("homeassistant/switch/" + place + "/switch2/state").c_str(), "OFF", true);
     }
   }
-  if(strcmp(topic,"place/switch3")==0){
-    if ( message == "on" ) {
-      digitalWrite(switch3,HIGH);
-      client.publish("place/switch3/state", "on");  
-    } else if (message == "off"){
-      digitalWrite(switch3,LOW);
-      client.publish("place/switch3/state", "off"); 
+  if (strcmp(topic, ("homeassistant/switch/" + place + "/switch3/set").c_str()) == 0){
+    if (message == "ON")
+    {
+      digitalWrite(switch3, HIGH);
+      client.publish(("homeassistant/switch/" + place + "/switch3/state").c_str(), "ON", true);
+    }
+    else if (message == "OFF"){
+      digitalWrite(switch3, LOW);
+      client.publish(("homeassistant/switch/" + place + "/switch3/state").c_str(), "OFF", true);
     }
   }
-  if (strcmp(topic,"place/switch4")==0){
-    if ( message == "on" ) {
-      digitalWrite(switch4,HIGH); 
-      client.publish("place/switch4/state", "on"); 
-    } else if (message == "off"){
-      digitalWrite(switch4,LOW);
-      client.publish("place/switch4/state", "off");
+  if (strcmp(topic, ("homeassistant/switch/" + place + "/switch4/set").c_str()) == 0)
+  {
+    if (message == "ON"){
+      digitalWrite(switch4, HIGH);
+      client.publish(("homeassistant/switch/" + place + "/switch4/state").c_str(), "ON", true);
+    }
+    else if (message == "OFF"){
+      digitalWrite(switch4, LOW);
+      client.publish(("homeassistant/switch/" + place + "/switch4/state").c_str(), "OFF", true);
     }
   }
 }
